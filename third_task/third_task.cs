9ffcad6deaf7d919e7a10b1b3e30d691651ffcc9
LeipreachanTeo <pokiboom@mail.ru>
2022-09-12 20:47:38 +0700

    third task

---
+++ third_task/third_task.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите количество строк: ");
+int count_line = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int count_columns = Int32.Parse(Console.ReadLine());
+
+int[,] two_dim_array = new int [count_line , count_columns];
+
+Random rdm = new Random();
+for (int i = 0; i < count_line; i++)
+{
+    for (int j = 0; j < count_columns; j++)
+    {
+        two_dim_array[i, j] = rdm.Next(10);
+        Console.Write(two_dim_array[i,j]+ " ");
+    }
+    Console.WriteLine();
+}
+
+for(int j = 0; j < count_columns; j++)
+{
+    double sum = 0;
+    double average;
+    for (int i = 0; i < count_line; i++)
+    {
+        sum += two_dim_array[i,j];
+    }
+    average = sum / count_line;
+    Console.WriteLine($"Среднее арифметическое {j+1} столбца {Math.Round(average,1)} ");
+}
